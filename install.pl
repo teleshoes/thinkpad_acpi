b1b0ec0a3e8f5c306eb4c0d74c5223b4c32d5db0
Elliot Wolk <elliot.wolk@gmail.com>
2013-09-14 19:16:58 -0400

    pull install to a sub

--- install.pl
+++ install.pl
@@ -2,6 +2,9 @@
 use strict;
 use warnings;
 
+sub install($);
+
+my $mod = "thinkpad_acpi.ko";
 my $modprobeConfFile = "/etc/modprobe.d/thinkpad_acpi.conf";
 my $modprobeOpts = "options thinkpad_acpi fan_control=1\n";
 
@@ -26,37 +29,39 @@ sub main(@){
   $ENV{PWD} = "$ENV{PWD}/$srcDir";
 
   my $dir = "/lib/modules/$kernel/kernel/drivers/platform/x86/";
-  my $mod = "thinkpad_acpi.ko";
 
   system "patch thinkpad_acpi.c led.patch";
   system "make";
   system "patch -R thinkpad_acpi.c led.patch";
 
-  if(-e $mod){
-    print "\n\nsuccess!\n";
-    my $now = `date +%s`;
-    chomp $now;
-    my $bak = "$mod.orig.$now";
-    print "copying $mod to $dir\nbackup in $bak\n";
-    system "sudo mv $dir/$mod $dir/$bak";
-    system "sudo cp $mod $dir";
-
-    print "\n\n";
-    print "replacing $modprobeConfFile:\n";
-    system "cat", $modprobeConfFile;
-    print "with:\n";
-    open FH, "| sudo tee $modprobeConfFile";
-    print FH $modprobeOpts;
-    close FH;
-    print "\n\n";
-
-    print "remove and add module thinkpad_acpi\n";
-    system "sudo modprobe -r thinkpad_acpi";
-    system "sudo modprobe thinkpad_acpi";
-  }
+  install $dir if -e $mod;
 
   print "Cleaning..\n";
   system "make clean";
+}
+
+sub install($){
+  my $dir = shift;
+  print "\n\nsuccess!\n";
+  my $now = `date +%s`;
+  chomp $now;
+  my $bak = "$mod.orig.$now";
+  print "copying $mod to $dir\nbackup in $bak\n";
+  system "sudo mv $dir/$mod $dir/$bak";
+  system "sudo cp $mod $dir";
+
+  print "\n\n";
+  print "replacing $modprobeConfFile:\n";
+  system "cat", $modprobeConfFile;
+  print "with:\n";
+  open FH, "| sudo tee $modprobeConfFile";
+  print FH $modprobeOpts;
+  close FH;
+  print "\n\n";
+
+  print "remove and add module thinkpad_acpi\n";
+  system "sudo modprobe -r thinkpad_acpi";
+  system "sudo modprobe thinkpad_acpi";
 }
 
 &main(@ARGV);
