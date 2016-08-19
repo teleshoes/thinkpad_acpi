b53d2ea8d9458fcde8f71176db5bc38fd2641852
Elliot Wolk <elliot.wolk@gmail.com>
2016-08-19 16:25:51 -0400

    gen-new-version.pl: script to update thinkpad_acpi.c from kernel src tree

---
+++ gen-new-version.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub run(@);
+
+my $kernelSrc = "$ENV{HOME}/Code/linux/drivers/platform/x86/thinkpad_acpi.c";
+
+sub main(@){
+  die "Usage: $0 OLD_VERSION NEW_VERSION\n" if @_ != 2;
+  my ($oldVersion, $newVersion) = @_;
+  die "invalid old version: $oldVersion\n" if $oldVersion !~ /^\d+(\.\d+)*$/;
+  die "invalid new version: $newVersion\n" if $newVersion !~ /^\d+(\.\d+)*$/;
+  if(not -d $oldVersion){
+    die "missing $oldVersion\n";
+  }elsif(-d $newVersion){
+    die "$newVersion already exists\n";
+  }
+
+  run "mkdir", "$newVersion";
+
+  run "cp", "$oldVersion/thinkpad_acpi.c", "$newVersion/old.c";
+  run "cp", "$oldVersion/led.patch", "$newVersion/old.patch";
+
+  run "cp", $kernelSrc, "$newVersion/kernel.c";
+
+  run "cp", "$newVersion/old.c", "$newVersion/old_patched.c";
+  run "patch", "-p0", "$newVersion/old_patched.c", "$newVersion/old.patch";
+  die "ERROR: cannot patch $oldVersion\n" if $? != 0;
+
+  run "cp", "$newVersion/kernel.c", "$newVersion/kernel_patched.c";
+  run "patch", "-p0", "$newVersion/kernel_patched.c", "$newVersion/old.patch";
+  if($? != 0){
+    print "\n\nWARNING: old patch failed\n";
+  }else{
+    run "rm", "-f", "kernel_patched.c.orig";
+  }
+
+  print "compare $newVersion/old_patched.c and $newVersion/kernel_patched.c\n";
+  print "modify the file 'kernel_patched.c' to be correct, and then push enter to continue: ";
+  <STDIN>;
+
+  run "diff $newVersion/kernel.c $newVersion/kernel_patched.c > $newVersion/led.patch";
+  run "cp $newVersion/kernel.c $newVersion/thinkpad_acpi.c";
+}
+
+sub run(@){
+  print "@_\n";
+  system @_;
+}
+
+&main(@ARGV);
